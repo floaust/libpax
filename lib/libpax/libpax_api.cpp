7af060d6a6d1e1bbed80589834df5983c51047b6
floaust <flo.extra.email@gmail.com>
2022-06-01 22:30:00 +0200

    add callback func to struct

--- lib/libpax/libpax_api.cpp
+++ lib/libpax/libpax_api.cpp
@@ -28,14 +28,13 @@ limitations under the License.
 #include <esp_wifi.h>
 #include <string.h>
 #include "freertos/task.h"    // needed for tasks
-#include "freertos/timers.h"  // TimerHandle_t
 
-void (*report_callback)(void);
 struct count_payload_t* pCurrent_count;
 int counter_mode;
 int count_amount_max;
 int count_amount;
 int highest_count_amount;
+void (*ble_callback)(void);
 
 void fill_counter(struct count_payload_t* pCount) {
   pCount->ble_count = libpax_ble_counter_count();
@@ -45,41 +44,26 @@ void libpax_counter_reset() {
   if (highest_count_amount < macs_ble) {
     highest_count_amount = macs_ble;
   }
-  
+
   macs_ble = 0;
   ESP_LOGI(TAG, "reset");
   reset_bucket();
 
-  if (count_amount_max != 0)
-  {
+  if (count_amount_max != 0) {
     count_amount += 1;
     ESP_LOGI(TAG, "BLE scan count: %d", count_amount);
-    if (count_amount_max == count_amount)
-    {
+    if (count_amount_max == count_amount) {
       ESP_LOGI(TAG, "Highest BLE scan count: %d", highest_count_amount);
       libpax_counter_stop();
+      ble_callback();
     }
-    
   }
-  
 }
 
-void report(TimerHandle_t xTimer) {
-  ESP_LOGI(TAG, "BLE device count: %d", libpax_ble_counter_count());
-  libpax_counter_reset();
-}
-
-TimerHandle_t PaxReportTimer = NULL;
-int libpax_counter_init(void (*init_callback)(void),
-                        struct count_payload_t* init_current_count,
+int libpax_counter_init(struct count_payload_t* init_current_count,
                         uint16_t init_pax_report_interval_sec,
                         int init_counter_mode) {
-  if (PaxReportTimer != NULL && xTimerIsTimerActive(PaxReportTimer)) {
-    ESP_LOGW("initialization", "lib already active. Ignoring new init.");
-    return -1;
-  }
 
-  report_callback = init_callback;
   pCurrent_count = init_current_count;
   counter_mode = init_counter_mode;
 
@@ -93,28 +77,21 @@ int libpax_counter_init(void (*init_callback)(void),
   ESP_ERROR_CHECK(ret);
 
   libpax_counter_reset();
-
-  PaxReportTimer = xTimerCreate(
-      "PaxReportTimer", pdMS_TO_TICKS(init_pax_report_interval_sec * 1000),
-      pdTRUE, (void*)0, report);
-  xTimerStart(PaxReportTimer, 0);
   return 0;
 }
 
 int libpax_counter_start(libpax_config_t configuration) {
   if (configuration.blecounter) {
     set_BLE_rssi_filter(configuration.ble_rssi_threshold);
-    start_BLE_scan(configuration.blescanwindow,
-                   configuration.blescaninterval);
+    start_BLE_scan(configuration.blescanwindow, configuration.blescaninterval);
     count_amount_max = configuration.ble_scan_count;
+    ble_callback = configuration.ble_callback;
   }
   return 0;
 }
 
 int libpax_counter_stop() {
   stop_BLE_scan();
-  xTimerStop(PaxReportTimer, 0);
-  PaxReportTimer = NULL;
   ESP_LOGI(TAG, "stopped!");
   return 0;
 }
--- lib/libpax/libpax_api.h
+++ lib/libpax/libpax_api.h
@@ -6,31 +6,33 @@
 // Build-time options
 // #define LIBPAX_BLE  // enables BLE sniffing features in build
 
-#define LIBPAX_ERROR_BLE_NOT_AVAILABLE  0b00000010
+#define LIBPAX_ERROR_BLE_NOT_AVAILABLE 0b00000010
 
 // configuration given to lib for sniffing parameters
 struct libpax_config_t {
-    int ble_rssi_threshold;                 // Filter for how strong the bluetooth signal should be to be counted
-    uint8_t blecounter;                     // set to 0 if you do not want to install the BLE sniffer
-    uint16_t blescanwindow;                 // [milliseconds] scan window, see below, 3 ... 10240, default 80ms
-    uint16_t blescaninterval;               // [milliseconds] scan interval, see below, 3 ... 10240, default 80ms = 100% duty cycle
-    int ble_scan_count;                     // number of scans to be done, 0 means infinite [default]
+  int ble_rssi_threshold;       // Filter for how strong the bluetooth signal should be to be counted
+  uint8_t blecounter;           // set to 0 if you do not want to install the BLE sniffer
+  uint16_t blescanwindow;       // [milliseconds] scan window, see below, 3 ... 10240, default 80ms
+  uint16_t blescaninterval;     // [milliseconds] scan interval, see below, 3 ... 10240, default 80ms = 100% duty cycle
+  int ble_scan_count;           // number of scans to be done, 0 means infinite [default]
+  void (*ble_callback)(void);   // callback function for executing after stopping BLE counter
 };
-
 
 // payload updated periodically or on demand
 struct count_payload_t {
-    uint32_t ble_count;  // detected ble_count in interval
+  uint32_t ble_count;  // detected ble_count in interval
 };
 
 /**
- *   Must be called before use of the lib. Initialze a callback the payload paxcount is written back too.
- *   @param[in] callback Callback which is called every pax_report_interval_sec to inform on current pax
- *   @param[out] current_count memory for pax count. Updated directly before callback is called
- *   @param[in] pax_report_interval_sec defines interval in s between a pax count callback.
+ *   Must be called before use of the lib.
+ *   @param[out] current_count memory for pax count. Updated directly before
+ * callback is called
+ *   @param[in] pax_report_interval_sec defines interval in s between a pax
+ * count callback.
  *   @param[in] countermode avalible modes TBD
-*/
-int libpax_counter_init(void (*callback)(void), struct count_payload_t* current_count, uint16_t pax_report_interval_sec, int countermode);
+ */
+int libpax_counter_init(struct count_payload_t* current_count,
+                        uint16_t pax_report_interval_sec, int countermode);
 
 /**
  *   Starts hardware wifi layer and counting of pax
@@ -38,7 +40,8 @@ int libpax_counter_init(void (*callback)(void), struct count_payload_t* current_
 int libpax_counter_start(libpax_config_t configuration);
 
 /**
- *   Stops sniffing process after which no new macs will be received and the wifi allocation is shutdown
+ *   Stops sniffing process after which no new macs will be received and the
+ * wifi allocation is shutdown
  */
 int libpax_counter_stop();
 
@@ -53,6 +56,6 @@ int libpax_counter_count(struct count_payload_t* count);
  */
 #define LIBPAX_CONFIG_SIZE 64
 
-//disables wifi chip
+// disables wifi chip
 void disable_wifi();
 #endif
